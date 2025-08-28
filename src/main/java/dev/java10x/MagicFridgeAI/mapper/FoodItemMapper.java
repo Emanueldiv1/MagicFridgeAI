f2b036dcfd53ac6f779e9a6d87c43557f8492e75
Emanuel <fergimanu3@gmail.com>
2025-08-28 20:56:40 -0300

    feat: adicionar classe de mapeamento FoodItemModel (DTO <-> Model)

---
+++ src/main/java/dev/java10x/MagicFridgeAI/mapper/FoodItemMapper.java
@@ -0,0 +1,34 @@
+package dev.java10x.MagicFridgeAI.mapper;
+
+
+import dev.java10x.MagicFridgeAI.dto.FoodItemDTO;
+import dev.java10x.MagicFridgeAI.model.FoodItemModel;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FoodItemMapper {
+
+    public FoodItemModel map(FoodItemDTO foodItemDTO){
+        FoodItemModel foodItemModel = new FoodItemModel();
+
+        foodItemModel.setId(foodItemDTO.getId());
+        foodItemModel.setNome(foodItemDTO.getNome());
+        foodItemModel.setCategoria(foodItemDTO.getCategoria());
+        foodItemModel.setQuantidade(foodItemDTO.getQuantidade());
+        foodItemModel.setValidade(foodItemDTO.getValidade());
+
+        return foodItemModel;
+    }
+
+    public FoodItemDTO map(FoodItemModel foodItemModel) {
+        FoodItemDTO foodItemDTO = new FoodItemDTO();
+
+        foodItemDTO.setId(foodItemModel.getId());
+        foodItemDTO.setNome(foodItemModel.getNome());
+        foodItemDTO.setCategoria(foodItemModel.getCategoria());
+        foodItemDTO.setQuantidade(foodItemModel.getQuantidade());
+        foodItemDTO.setValidade(foodItemModel.getValidade());
+
+        return foodItemDTO;
+    }
+}
