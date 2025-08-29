5c0cd53e15db14a4ec22c29f86c09b621aa3a440
Emanuel <fergimanu3@gmail.com>
2025-08-28 21:59:41 -0300

    feat: listar categorias do meu enum, e cadastrar alimentos

--- src/main/java/dev/java10x/MagicFridgeAI/controller/FoodItemController.java
+++ src/main/java/dev/java10x/MagicFridgeAI/controller/FoodItemController.java
@@ -1,10 +1,13 @@
 package dev.java10x.MagicFridgeAI.controller;
 
 
+import dev.java10x.MagicFridgeAI.dto.FoodItemDTO;
+import dev.java10x.MagicFridgeAI.enums.FoodCategory;
 import dev.java10x.MagicFridgeAI.service.FoodItemService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/food")
@@ -13,6 +16,15 @@ public class FoodItemController {
     @Autowired
     private FoodItemService foodItemService;
 
+    @PostMapping("/register")
+    public ResponseEntity <FoodItemDTO> criar(@RequestBody FoodItemDTO foodItemDTO){
+        FoodItemDTO newFood = foodItemService.registerFood(foodItemDTO);
+        return ResponseEntity.ok(newFood);
+    }
 
+    @GetMapping("/categorias")
+    public FoodCategory[] listarCategorias() {
+        return FoodCategory.values();
+    }
 
 }
