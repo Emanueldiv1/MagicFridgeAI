473b76320a1653023096dd136b4bae95ec86266f
Emanuel <fergimanu3@gmail.com>
2025-08-28 21:57:09 -0300

    feat: criar registerFood na classe FoodItemService

--- src/main/java/dev/java10x/MagicFridgeAI/service/FoodItemService.java
+++ src/main/java/dev/java10x/MagicFridgeAI/service/FoodItemService.java
@@ -1,4 +1,26 @@
 package dev.java10x.MagicFridgeAI.service;
 
+import dev.java10x.MagicFridgeAI.Repository.FoodItemRepository;
+import dev.java10x.MagicFridgeAI.dto.FoodItemDTO;
+import dev.java10x.MagicFridgeAI.mapper.FoodItemMapper;
+import dev.java10x.MagicFridgeAI.model.FoodItemModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class FoodItemService {
+
+    @Autowired
+    private FoodItemRepository foodItemRepository;
+
+    @Autowired
+    private FoodItemMapper foodItemMapper;
+
+    public FoodItemDTO registerFood(FoodItemDTO foodItemDTO){
+        FoodItemModel foodItemModel = foodItemMapper.map(foodItemDTO);
+        foodItemRepository.save(foodItemModel);
+        return foodItemMapper.map(foodItemModel);
+    }
+
+
 }
