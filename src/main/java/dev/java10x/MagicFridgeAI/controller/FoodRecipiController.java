25122f6a7f1df924adcdfce8138469ab60725c63
Emanuel <fergimanu3@gmail.com>
2025-09-24 20:19:39 -0300

    feat: Criar endpoint para API para sugestões de receitas com AI

--- src/main/java/dev/java10x/MagicFridgeAI/controller/FoodRecipiController.java
+++ src/main/java/dev/java10x/MagicFridgeAI/controller/FoodRecipiController.java
@@ -1,7 +1,32 @@
 package dev.java10x.MagicFridgeAI.controller;
 
+import dev.java10x.MagicFridgeAI.dto.FoodItemDTO;
+import dev.java10x.MagicFridgeAI.service.FoodItemService;
+import dev.java10x.MagicFridgeAI.service.GeminiService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @RestController
 public class FoodRecipiController {
+
+    @Autowired
+    private GeminiService geminiService;
+
+    @Autowired
+    private FoodItemService foodItemService;
+
+    @GetMapping("generate")
+    public Mono<ResponseEntity<String>> generatRecipe(){
+        List<FoodItemDTO> foodItem = foodItemService.ListarItens();
+        return geminiService.generatRecipe(foodItem)
+                .map(recipe -> ResponseEntity.ok(recipe))
+                .defaultIfEmpty(ResponseEntity.noContent().build());
+    }
+
 }
